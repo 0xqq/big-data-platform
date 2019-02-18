7072ecd50ea41df9c07a28ee9f9a6962754d6b22
OrangeJ <juhaotian@foxmail.com>
2019-02-18 23:12:42 +0800

    [feat] 初步添加Header组件，已可正确使用module.scss

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import styles from './index.module.scss';
+
+export default class Header extends Component {
+    render () {
+        return (
+            <div className={styles.Header}>Header</div>
+        );
+    }
+}
